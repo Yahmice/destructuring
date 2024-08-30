f3187b82fcda075dbdff0ad9a7f8fb40f24cd11d
Psycho <ilya.efremov.0498@gmail.com>
2024-08-30 22:54:57 +0500

    Init

---
+++ src/js/app.js
@@ -0,0 +1,34 @@
+const character = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон'
+    }, 
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...'
+      // <- обратите внимание, описание "засекречено"
+    }
+  ]	
+}
+
+function extractInfo() {
+  return character.special.map(({ id, icon, description }) => ({
+    id,
+    icon,
+    description: description || "Описание недоступно"
+  }))
+};
+
+console.log(extractInfo());
+
+export default extractInfo;
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('node:path');
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+}
